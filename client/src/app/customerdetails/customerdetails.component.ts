61c87ecf44083238874d5edd3bb2054caa5e4b21
Manish Shaw <manishshaw323@gmail.com>
2019-05-06 11:17:34 +0530

    updated components

--- client/src/app/customerdetails/customerdetails.component.ts
+++ client/src/app/customerdetails/customerdetails.component.ts
@@ -10,39 +10,26 @@ import { Router } from '@angular/router'
   styleUrls: ['./customerdetails.component.css']
 })
 export class CustomerdetailsComponent implements OnInit {
-  allCustomerDetails: CustomerDetails[] = [{id:1, name:"Manish Shaw", email:"abc@test.com", phone:"1356842612", city:"Bangalore", state:"Karnataka", country:"India", organization:"Company1", jobProfile:"Developer", additionalInfo:"Very interested in the products"}, 
-                         {id:2, name:"Name2", email:"abcd@test.com", phone:"37843784634", city:"Mangalore", state:"Karnataka", country:"India", organization:"Company2", jobProfile:"Analyst", additionalInfo:"Very interested in the products but buys rarely"}, 
-                         {id:3, name:"Name3", email:"abcde@test.com", phone:"6786877898", city:"Chennai", state:"Tamil Nadu", country:"India", organization:"Company3", jobProfile:"CEO", additionalInfo:"Has Bought Products before"}];
-
   customerDetails: CustomerDetails = null;
+
   constructor(private dataService: DataService, private activeRoute: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     this.getCustomerDetails();
   }
 
-//  getCustomerDetails(): void {
-//    this.activeRoute.params.subscribe(routeParams => {
-//
-//      this.dataService.getCustomerDetails(routeParams.id).subscribe(
-//
-//        (customerDetails: CustomerDetails) => {
-//          this.customerDetails = customerDetails;
-//          console.log(customerDetails);
-//        });
-//
-//    });
-    
-    getCustomerDetails(): void {
-        this.activeRoute.params.subscribe(routeParams => {
-            this.allCustomerDetails.filter((x) => 
-            { 
-                if(x.id.toString() === routeParams.id) {
-                    this.customerDetails = x;
-                    console.log(this.customerDetails);
-                }
-            });
+  getCustomerDetails(): void {
+    this.activeRoute.params.subscribe(routeParams => {
+
+      this.dataService.getCustomerDetails(routeParams.id).subscribe(
+
+        (customerDetails: CustomerDetails) => {
+          this.customerDetails = customerDetails;
+          console.log(customerDetails);
         });
+
+    });
+
   }
 
   goToCustomerPage() : void {
--- client/src/app/customers/customers.component.ts
+++ client/src/app/customers/customers.component.ts
@@ -9,28 +9,20 @@ import { Router } from '@angular/router'
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers = [
-               {id:1, name:"Manish Shaw", email:"abc@test.com", phone:"1356842612"},
-               {id:2, name:"Name2", email:"abcd@test.com", phone:"37843784634"},
-               {id:3, name:"Name3", email:"abcde@test.com", phone:"6786877898"}
-              ];
+  customers: Customer[] = [];
   constructor(private dataService: DataService, private router:Router) { }
-  
+
   ngOnInit() {
     this.getCustomerList();
   }
-  
-//  getCustomerList(): void {
-//    this.dataService.getCustomerList().subscribe(
-//      (customers: Customer[]) => {
-//        this.customers = customer;
-//        console.log(customers)
-//      });
-//  }
-  
+
   getCustomerList(): void {
-      console.log(this.customers);
-    }
+    this.dataService.getCustomerList().subscribe(
+      (customers: Customer[]) => {
+        this.customers = customers;
+        console.log(customers)
+      });
+  }
 
   goToDetailsPage(id : number) : void {
       this.router.navigateByUrl("/customerdetails/"+id);
